557a914e77c0caa259488d5104b6c6c67642e230
Tim Miller <innerlogic4321@gmail.com>
2021-01-11 12:31:29 -0500

    Add Android Image Picker

--- ImageUpload.Mobile/ImageUpload.Mobile.Android/AndroidPlatformProperties.cs
+++ ImageUpload.Mobile/ImageUpload.Mobile.Android/AndroidPlatformProperties.cs
@@ -16,6 +16,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Drastic.Common.Interfaces;
+using ImageUpload.Mobile.Droid;
 
 namespace Drastic.Forms.Android
 {
@@ -56,7 +57,21 @@ namespace Drastic.Forms.Android
 
         public Task<Stream> PickImageAsync()
         {
-            throw new NotImplementedException();
+            // Define the Intent for getting images
+            Intent intent = new Intent();
+            intent.SetType("image/*");
+            intent.SetAction(Intent.ActionGetContent);
+
+            // Start the picture-picker activity (resumes in MainActivity.cs)
+            MainActivity.Instance.StartActivityForResult(
+                Intent.CreateChooser(intent, "Select Picture"),
+                MainActivity.PickImageId);
+
+            // Save the TaskCompletionSource object as a MainActivity property
+            MainActivity.Instance.PickImageTaskCompletionSource = new TaskCompletionSource<Stream>();
+
+            // Return Task object
+            return MainActivity.Instance.PickImageTaskCompletionSource.Task;
         }
     }
 }
--- ImageUpload.Mobile/ImageUpload.Mobile.Android/MainActivity.cs
+++ ImageUpload.Mobile/ImageUpload.Mobile.Android/MainActivity.cs
@@ -3,7 +3,10 @@
 // </copyright>
 
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
@@ -13,9 +16,28 @@ using Drastic.Forms.Android;
 
 namespace ImageUpload.Mobile.Droid
 {
+    /// <summary>
+    /// Main Activity for app.
+    /// </summary>
     [Activity(Label = "ImageUpload.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        /// <summary>
+        /// Field, property, and method for Picture Picker.
+        /// </summary>
+        public static readonly int PickImageId = 1000;
+
+        /// <summary>
+        /// Gets or sets the Pick Image Task Completion Source.
+        /// </summary>
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { get; set; }
+
+        /// <summary>
+        /// Gets the main activity instance.
+        /// </summary>
+        internal static MainActivity Instance { get; private set; }
+
+        /// <inheritdoc/>
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -23,16 +45,39 @@ namespace ImageUpload.Mobile.Droid
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
+        /// <inheritdoc/>
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            Instance = this;
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Forms9Patch.Droid.Settings.Initialize(this);
             var builder = new ContainerBuilder();
             builder.RegisterType<AndroidPlatformProperties>().As<IPlatformProperties>();
             this.LoadApplication(new App(builder));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = this.ContentResolver.OpenInputStream(uri);
+
+                    // Set the Stream as the completion of the Task
+                    this.PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    this.PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
         }
     }
 }
